a15c5c6620ee05f1e8b4769bea0523f2b8992a5a
Andrea Junia <andre3aj.f@gmail.com>
2021-12-23 16:11:11 -0300

    alteracoes fontes e curriculo

--- js/language.js
+++ js/language.js
@@ -1,33 +1,70 @@
-function fChange() {
-const myNodelist = document.querySelectorAll("body");   
-console.log(myNodelist)  
+function fInicio() {
+    fChange(document.getElementById("bttlgg").innerHTML = "BR");
+}
+
+function fChange() { 
     if (document.getElementById("bttlgg").innerHTML == 'BR') {
+        
         document.getElementById("bttlgg").innerHTML = "EN";
         document.getElementById("banh1").innerHTML = "Olá Mundo! Eu Sou";
+
+        document.getElementById("me1").innerHTML = cAboutBR1; 
+        document.getElementById("me2").innerHTML = cAboutBR2; 
+        document.getElementById("me3").innerHTML = cAboutBR3; 
+
         document.getElementById("edc").innerHTML = edcBR; 
-        document.getElementById("exp").innerHTML = expBR; 
-        document.getElementById("ed1").innerHTML = ed1BR; 
-        document.getElementById("ed2").innerHTML = ed2BR;
-        document.getElementById("ed3").innerHTML = ed3BR;
-        document.getElementById("ed4").innerHTML = ed4BR;
-        document.getElementById("ex1").innerHTML = ex1BR; 
-        document.getElementById("ex2").innerHTML = ex2BR;
-        document.getElementById("ex3").innerHTML = ex3BR;
-        document.getElementById("ex4").innerHTML = ex4BR;
-        document.getElementById("me").innerHTML = cAboutBR; 
+        document.getElementById("exp").innerHTML = expBR;
+
+        document.getElementById("edc1").innerHTML = edc1BR;
+        document.getElementById("edc2").innerHTML = edc2BR;
+        document.getElementById("edc3").innerHTML = edc3BR;
+        document.getElementById("edc4").innerHTML = edc4BR;
+        document.getElementById("edc5").innerHTML = edc5BR;
+        document.getElementById("edc6").innerHTML = edc6BR;
+        document.getElementById("edc7").innerHTML = edc7BR;
+        document.getElementById("edc8").innerHTML = edc8BR;
+
+        document.getElementById("exp1").innerHTML = exp1BR;
+        document.getElementById("exp2").innerHTML = exp2BR;
+        document.getElementById("exp3").innerHTML = exp3BR;
+        document.getElementById("exp4").innerHTML = exp4BR;
+        document.getElementById("exp5").innerHTML = exp5BR;
+        document.getElementById("exp6").innerHTML = exp6BR;
+        document.getElementById("exp7").innerHTML = exp7BR;
+        document.getElementById("exp8").innerHTML = exp8BR;
+        document.getElementById("exp9").innerHTML = exp9BR;
+        document.getElementById("exp10").innerHTML = exp10BR;        
+
     } else {
         document.getElementById("bttlgg").innerHTML = "BR";
         document.getElementById("banh1").innerHTML = "Hello World! I'm";
+
+        document.getElementById("me1").innerHTML = cAboutEN1; 
+        document.getElementById("me2").innerHTML = cAboutEN2; 
+        document.getElementById("me3").innerHTML = cAboutEN3; 
+
         document.getElementById("edc").innerHTML = edcEN;
-        document.getElementById("exp").innerHTML = expEN; 
-        document.getElementById("ed1").innerHTML = ed1EN;
-        document.getElementById("ed2").innerHTML = ed2EN;
-        document.getElementById("ed3").innerHTML = ed3EN;
-        document.getElementById("ed4").innerHTML = ed4EN;
-        document.getElementById("ex1").innerHTML = ex1EN; 
-        document.getElementById("ex2").innerHTML = ex2EN;
-        document.getElementById("ex3").innerHTML = ex3EN;
-        document.getElementById("ex4").innerHTML = ex4EN;
-        document.getElementById("me").innerHTML = cAboutEN;
+        document.getElementById("exp").innerHTML = expEN;
+
+        document.getElementById("edc1").innerHTML = edc1EN;
+        document.getElementById("edc2").innerHTML = edc2EN;
+        document.getElementById("edc3").innerHTML = edc3EN;
+        document.getElementById("edc4").innerHTML = edc4EN;
+        document.getElementById("edc5").innerHTML = edc5EN;
+        document.getElementById("edc6").innerHTML = edc6EN;
+        document.getElementById("edc7").innerHTML = edc7EN;
+        document.getElementById("edc8").innerHTML = edc8EN;
+
+        document.getElementById("exp1").innerHTML = exp1EN;
+        document.getElementById("exp2").innerHTML = exp2EN;
+        document.getElementById("exp3").innerHTML = exp3EN;
+        document.getElementById("exp4").innerHTML = exp4EN;
+        document.getElementById("exp5").innerHTML = exp5EN;
+        document.getElementById("exp6").innerHTML = exp6EN;
+        document.getElementById("exp7").innerHTML = exp7EN;
+        document.getElementById("exp8").innerHTML = exp8EN;
+        document.getElementById("exp9").innerHTML = exp9EN;
+        document.getElementById("exp10").innerHTML = exp10EN;
     }    
+
 }
